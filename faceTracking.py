03fa520af02a48489ef6b231e5e1a5640dac9c65
Ajith Valappil <ajith.vv@gmail.com>
2015-01-18 20:41:26 -0600

    Starting

---
+++ faceTracking.py
@@ -0,0 +1,38 @@
+from SimpleCV import *
+import time
+"""
+This is an example of HOW-TO use FaceRecognizer to recognize gender
+of the person.
+"""
+cascade = LAUNCH_PATH + "/" + "Features/HaarCascades/face.xml"
+
+f = FaceRecognizer()
+cam = Camera()
+
+def identifyGender():
+    time.sleep(1)
+    img = cam.getImage().flipHorizontal().scale(0.5)
+    feat = img.findHaarFeatures(cascade)
+    if feat:
+        i = 0
+        for trovato in feat:
+            i = i + 1
+        crop_image = feat.sortArea()[-1].crop()
+        #feat.sortArea()[-1].draw()
+        f.load(LAUNCH_PATH + "/" + "Features/FaceRecognizerData/GenderData.xml")
+        w, h = f.imageSize
+        crop_image = crop_image.resize(w, h)
+        label, confidence = f.predict(crop_image)
+        if label == 0:
+            print "Female"
+            img.drawText("Female" +  str(i) + " faces", fontsize=24)
+        else:
+            print "Male"
+            img.drawText("Male"  +  str(i) + " faces", fontsize=24)
+    else:
+        img.drawText("No face detected", fontsize=48)
+    img.show()
+
+keeprunning = 0
+while keeprunning==0:
+    identifyGender()
