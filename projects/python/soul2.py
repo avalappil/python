e6b41e9f80e2cc05efa635febbcbf47c8003325c
Ajith Valappil <ajith.vv@gmail.com>
2015-01-27 00:54:59 -0600

    Tue 12:54

--- projects/python/soul2.py
+++ projects/python/soul2.py
@@ -8,23 +8,64 @@ from webiopi.devices.serial import Serial
 serial = Serial("ttyAMA0", 9600)
 
 def setup():
-    os.system ("espeak 'hi'")
-    print ("Starting")
+    os.system ("espeak 'Started'")
     # empty input buffer before starting processing
     while (serial.available() > 0):
         serial.readString()
-
-#def loop():
-#def destroy():
-
-#    if (serial.available() > 0):
-#        line = serial.readString()     # read available data
-#        print (line)
 #    webiopi.sleep(1)
 
 @webiopi.macro
 def loadData(str):
     print (str)
-    #os.system("espeak 'got data " + str + " ' ")
-# Program will clean up all GPIO settings and terminates
-# End
+    if (str == "a"):
+    	serial.write("PL 1 SQ 14 ONCE<CR>")
+    elif (str == "b"):
+    	serial.write("PL 1 SQ 23 ONCE<CR>")
+    elif (str == "c"):
+    	serial.write("PL 1 SQ 15 ONCE<CR>")
+    elif (str == "d"):
+    	serial.write("PL 1 SQ 4 ONCE<CR>")
+    elif (str == "e"):
+    	serial.write("PL 1 SQ 5 ONCE<CR>")
+    elif (str == "f"):
+    	serial.write("PL 1 SQ 12 ONCE<CR>")
+    elif (str == "g"):
+    	serial.write("PL 1 SQ 0 ONCE<CR>")
+    elif (str == "h"):
+    	serial.write("PL 1 SQ 13 ONCE<CR>")
+    elif (str == "i"):
+    	serial.write("PL 1 SQ 9 ONCE<CR>")
+    elif (str == "j"):
+    	serial.write("PL 1 SQ 8 ONCE<CR>")
+    elif (str == "k"):
+    	serial.write("PL 1 SQ 1 ONCE<CR>")
+    elif (str == "l"):
+    	serial.write("PL 1 SQ 2 ONCE<CR>")
+    elif (str == "m"):
+    	serial.write("PL 1 SQ 3 ONCE<CR>")
+    elif (str == "n"):
+    	serial.write("PL 1 SQ 16 ONCE<CR>")
+    elif (str == "o"):
+    	serial.write("PL 1 SQ 17 ONCE<CR>")
+    elif (str == "p"):
+    	serial.write("PL 1 SQ 19 ONCE<CR>")
+    elif (str == "q"):
+    	serial.write("PL 1 SQ 21 ONCE<CR>")
+    elif (str == "r"):
+    	serial.write("PL 1 SQ 1 ONCE<CR>")
+    elif (str == "s"):
+    	serial.write("PL 1 SQ 10 ONCE<CR>")
+    elif (str == "t"):
+    	serial.write("PL 1 SQ 20 ONCE<CR>")
+    elif (str == "u"):
+    	serial.write("PL 1 SQ 6 ONCE<CR>")
+    elif (str == "v"):
+    	serial.write("PL 1 SQ 7 ONCE<CR>")
+    #elif (str == "w"):
+    	#serial.write("PL 1 SQ 1 ONCE<CR>")
+    #elif (str == "x"):
+    	#serial.write("PL 1 SQ 1 ONCE<CR>")
+    #elif (str == "y"):
+    	#serial.write("PL 1 SQ 1 ONCE<CR>")
+    #elif (str == "z"):
+    	#serial.write("PL 1 SQ 1 ONCE<CR>")
