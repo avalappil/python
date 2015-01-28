ac58fc0ad544a4751deda0ba9d0b90ac945bf7b7
Ajith Valappil <ajith.vv@gmail.com>
2015-01-27 21:44:33 -0600

    tue 9:44

--- projects/python/soul2.py
+++ projects/python/soul2.py
@@ -5,11 +5,15 @@ import subprocess
 # import Serial driver
 from webiopi.devices.serial import Serial
 
+headPos = 1500
+minPos = 550
+maxPos = 2400
 # initialize Serial driver
 serial = Serial("ttyAMA0", 9600)
-
 def setup():
-    os.system ("espeak 'Started'")
+    headPos = 1500
+    minPos = 550
+    maxPos = 2400
     # empty input buffer before starting processing
     while (serial.available() > 0):
         serial.readString()
@@ -17,51 +21,57 @@ def setup():
 @webiopi.macro
 def loadData(str):
     print (str)
-    if (str == "a"):
-    	serial.write(bytes("PL 1 SQ 14 ONCE\r", 'UTF-8'))
-    elif (str == "b"):
-    	serial.write(bytes("PL 1 SQ 23 ONCE\r", 'UTF-8'))
-    elif (str == "c"):
-    	serial.write(bytes("PL 1 SQ 15 ONCE\r", 'UTF-8'))
-    elif (str == "d"):
-    	serial.write(bytes("PL 1 SQ 4 ONCE\r", 'UTF-8'))
-    elif (str == "e"):
-    	serial.write(bytes("PL 1 SQ 5 ONCE\r", 'UTF-8'))
-    elif (str == "f"):
-    	serial.write(bytes("PL 1 SQ 12 ONCE\r", 'UTF-8'))
-    elif (str == "g"):
+    if (str == "0"):
     	serial.write(bytes("PL 1 SQ 0 ONCE\r", 'UTF-8'))
-    elif (str == "h"):
-    	serial.write(bytes("PL 1 SQ 13 ONCE\r", 'UTF-8'))
-    elif (str == "i"):
-    	serial.write(bytes("PL 1 SQ 9 ONCE\r", 'UTF-8'))
-    elif (str == "j"):
-    	serial.write(bytes("PL 1 SQ 8 ONCE\r", 'UTF-8'))
-    elif (str == "k"):
+    elif (str == "1"):
     	serial.write(bytes("PL 1 SQ 1 ONCE\r", 'UTF-8'))
-    elif (str == "l"):
+    elif (str == "2"):
     	serial.write(bytes("PL 1 SQ 2 ONCE\r", 'UTF-8'))
-    elif (str == "m"):
+    elif (str == "3"):
     	serial.write(bytes("PL 1 SQ 3 ONCE\r", 'UTF-8'))
-    elif (str == "n"):
-    	serial.write(bytes("PL 1 SQ 16 ONCE\r", 'UTF-8'))
-    elif (str == "o"):
-    	serial.write(bytes("PL 1 SQ 17 ONCE\r", 'UTF-8'))
-    elif (str == "p"):
-    	serial.write(bytes("PL 1 SQ 19 ONCE\r", 'UTF-8'))
-    elif (str == "q"):
-    	serial.write(bytes("PL 1 SQ 21 ONCE\r", 'UTF-8'))
-    elif (str == "r"):
-    	serial.write(bytes("PL 1 SQ 1 ONCE\r", 'UTF-8'))
-    elif (str == "s"):
-    	serial.write(bytes("PL 1 SQ 10 ONCE\r", 'UTF-8'))
-    elif (str == "t"):
-    	serial.write(bytes("PL 1 SQ 20 ONCE\r", 'UTF-8'))
-    elif (str == "u"):
+    elif (str == "4"):
+    	serial.write(bytes("PL 1 SQ 4 ONCE\r", 'UTF-8'))
+    elif (str == "5"):
+    	serial.write(bytes("PL 1 SQ 5 ONCE\r", 'UTF-8'))
+    elif (str == "6"):
     	serial.write(bytes("PL 1 SQ 6 ONCE\r", 'UTF-8'))
-    elif (str == "v"):
+    elif (str == "7"):
     	serial.write(bytes("PL 1 SQ 7 ONCE\r", 'UTF-8'))
-    #elif (str == "w"):
-    	#serial.write(bytes("PL 1 SQ 1 ONCE\r", 'UTF-8'))
+    elif (str == "8"):
+    	serial.write(bytes("PL 1 SQ 8 ONCE\r", 'UTF-8'))
+    elif (str == "9"):
+    	serial.write(bytes("PL 1 SQ 9 ONCE\r", 'UTF-8'))
+    elif (str == "10"):
+    	serial.write(bytes("PL 1 SQ 10 ONCE\r", 'UTF-8'))
+    elif (str == "11"):
+    	serial.write(bytes("PL 1 SQ 11 ONCE\r", 'UTF-8'))
+    elif (str == "12"):
+    	serial.write(bytes("PL 1 SQ 12 ONCE\r", 'UTF-8'))
+    elif (str == "13"):
+    	serial.write(bytes("PL 1 SQ 13 ONCE\r", 'UTF-8'))
+    elif (str == "14"):
+    	serial.write(bytes("PL 1 SQ 14 ONCE\r", 'UTF-8'))
+    elif (str == "15"):
+    	serial.write(bytes("PL 1 SQ 15 ONCE\r", 'UTF-8'))
+    elif (str == "16"):
+    	serial.write(bytes("PL 1 SQ 16 ONCE\r", 'UTF-8'))
+    elif (str == "17"):
+    	serial.write(bytes("PL 1 SQ 17 ONCE\r", 'UTF-8'))
+    elif (str == "18"):
+    	serial.write(bytes("PL 1 SQ 18 ONCE\r", 'UTF-8'))
+    elif (str == "19"):
+    	serial.write(bytes("PL 1 SQ 19 ONCE\r", 'UTF-8'))
+    elif (str == "20"):
+    	serial.write(bytes("PL 1 SQ 20 ONCE\r", 'UTF-8'))
+    elif (str == "l"):
+        headPos = headPos - 1
+        if (headPos < minPos):
+            headPos = minPos
+        serial.write(bytes("P#15 P" + str(headPos) +" T1000\r", 'UTF-8'))
+    elif (str == "r"):
+        headPos = headPos + 1
+        if (headPos > maxPos):
+            headPos = maxPos
+        serial.write(bytes("P#15 P" + str(headPos) +" T1000\r", 'UTF-8'))
     #elif (str == "x"):
     #elif (str == "y"):
--- projects/python/test.py
+++
@@ -1,2 +0,0 @@
-import subprocess
-subprocess.call("../videostream stream.sh")
