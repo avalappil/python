b9fb482fc9686c54663e1d8c1cb58931a9c83b19
Ajith Valappil <ajith.vv@gmail.com>
2015-02-01 22:43:38 -0600

    sun 10:43

--- projects/python/readFaces.py
+++ projects/python/readFaces.py
@@ -4,22 +4,21 @@ import sys
 import SimpleCV
 from SimpleCV import *
 import urllib
-import requests
 
 cascade = "/home/pi/Code/SimpleCV/SimpleCV/Features/HaarCascades/face.xml"
-URL = "http://127.0.0.1:8421/?action=snapshot"
+URL = "http://10.0.1.17:8421/?action=snapshot"
 imgName = "snapShotImg.jpg"
 faceCascade = cv2.CascadeClassifier(cascade)
 username = 'webiopi'
 password = 'raspberry'
 
-url = "http://127.0.1.1:8000/macros/setfaces/"
+url = "http://10.0.1.17:8000/macros/setfaces/"
 
-while True:
-	time.sleep(1)
+while True:	
 	try:
 		urllib.urlretrieve(URL, imgName)
-		time.sleep(2);	
+		time.sleep(2)
+		print ("Image Saved")
 		image = cv2.imread(imgName)
 		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -34,11 +33,6 @@ while True:
 		#print (len(faces))
 		str = url + str(len(faces))
 		print (str)
-		r = requests.post(str, auth=(username, password))
+		#r = requests.post(str, auth=(username, password))
 	except:
-		str = url + "0"
-		try:
-			r = requests.post(str, auth=(username, password))
-		except:
-			pass
 		pass
