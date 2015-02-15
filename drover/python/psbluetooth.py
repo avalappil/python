77779592ff0693513fed5c6d7a5ef6f7b8952856
Ajith Valappil <ajith.vv@gmail.com>
2015-02-15 01:11:43 -0600

    sun 1:11

--- drover/python/psbluetooth.py
+++ drover/python/psbluetooth.py
@@ -19,8 +19,11 @@ threshold = 0.30
 print 'Initialized Joystick : %s' % j.get_name()
 
 
-typeForArduino = "";
-dataForArduino = "";
+typeForArduino = ""
+dataForArduino = ""
+
+typeForServo = ""
+dataForServo = ""
 
 # Configure the motors to match the current settings.
 def drive(typ, data):
@@ -33,9 +36,7 @@ def drive(typ, data):
 try:
     # This is the main loop
     while True:
-      #time.sleep(0.5)
-      #print typeForArduino
-      #print dataForArduino
+
       events = pygame.event.get()
       for event in events:
         UpdateMotors = 0
@@ -53,59 +54,25 @@ try:
           elif event.axis == 3:
             forward = float("{0:.2f}".format(event.value))
             UpdateMotors = 1
-          ##### lef joy stick - camera control
-          elif event.axis == 1:
-            up = float("{0:.2f}".format(event.value))
-            UpdateServo = 1
-          elif event.axis == 0:
-            rotate = float("{0:.2f}".format(event.value))
-            UpdateServo = 1
-
-          if UpdateServo:
-            if (up > threshold):
-              print "up: "
-              print up
-            elif (up < -threshold):
-              print "down: "
-              print up
-            elif (rotate > threshold):
-              print "left: "
-              print rotate
-            elif (rotate < -threshold):
-              print "right: "
-              print rotate
-
-
 
           # Check if we need to update what the motors are doing
           if UpdateMotors:
             if (forward > threshold):
               print "reverse: "
               print forward
-              typeForArduino = "f"
-              dataForArduino = str(forward)
               drive("f",str(forward))
             elif (forward < -threshold):
               print "forward: "
               print  forward
-              typeForArduino = "f"
-              dataForArduino = str(forward)
               drive("f",str(forward))
             elif (turn > threshold):
               print "right: "
               print turn
-              typeForArduino = "t"
-              dataForArduino = str(turn)
               drive("t",str(turn))
             elif (turn < -threshold):
               print "left: "
               print turn
-              typeForArduino = "t"
-              dataForArduino = str(turn)
               drive("t",str(turn))
-            #else:
-              #typeForArduino = "t"
-              #dataForArduino = "0"
 
 except KeyboardInterrupt:
     # Turn off the motors
