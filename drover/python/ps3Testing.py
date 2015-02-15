0625ec9412845ab49599207f670744036cba1eda
Ajith Valappil <ajith.vv@gmail.com>
2015-02-15 16:16:07 -0600

    Sun 4:16

--- drover/python/ps3Testing.py
+++ drover/python/ps3Testing.py
@@ -14,7 +14,7 @@ pygame.init()
 j = pygame.joystick.Joystick(0)
 j.init()
 
-threshold = 30
+threshold = 0.30
 
 print 'Initialized Joystick : %s' % j.get_name()
 
@@ -26,17 +26,20 @@ typeForServo = ""
 dataForServo = ""
 
 # Configure the motors to match the current settings.
-def drive(typ, data):
-  print data
+def drive(typ,leftMotor,rightMotor):
   serialC.write(typ)
-  serialC.write(data)
+  serialC.write(leftMotor)
+  serialC.write("o")
+  serialC.write(rightMotor)
   serialC.write("#")
+  serialC.flush()
+
 
 # Try and run the main code, and in case of failure we can stop the motors
 try:
     # This is the main loop
     while True:
-      #time.sleep(2)
+      
       events = pygame.event.get()
       for event in events:
         direction = 0 #0 go front, else go back
@@ -51,57 +54,62 @@ try:
           turn = j.get_axis(2)
           forward = float("{0:.2f}".format(forward))
           turn = float("{0:.2f}".format(turn))
-          forward = forward * 100
-          turn = turn * 100
-          forward = int(round(float(forward)))
-          turn = int(round(float(turn)))
+          #forward = forward * 100
+          #turn = turn * 100
+          #forward = int(round(float(forward)))
+          #turn = int(round(float(turn)))
 
           if (forward < -threshold and turn < threshold and turn > -threshold):
             #go straight
             direction = 0
-            leftMotor = (255 * (forward * -1))/100
+            leftMotor = forward#(255 * (forward * -1))/100
             rightMotor = leftMotor
           elif (forward < -threshold and turn > threshold):
             #go slight right
             direction = 0
-            leftMotor = (255 * (forward * -1))/100
-            rightMotor = (255 * turn)/100
+            leftMotor = (forward * -1)#(255 * (forward * -1))/100
+            rightMotor = turn#(255 * turn)/100
           elif (forward < -threshold and turn < -threshold):
             #go slight left
             direction = 0
-            rightMotor = (255 * (forward * -1))/100
-            leftMotor = (255 * (turn * -1))/100
+            rightMotor = (forward * -1)#(255 * (forward * -1))/100
+            leftMotor = (turn * -1)#(255 * (turn * -1))/100
           elif (forward > threshold and turn < threshold and turn > -threshold):
             #go straight
             direction = 1
-            leftMotor = (255 * forward)/100
+            leftMotor = forward#(255 * forward)/100
             rightMotor = leftMotor
           elif (forward > threshold and turn > threshold):
             #go slight right
             direction = 1
-            leftMotor = (255 * forward)/100
-            rightMotor = (255 * turn)/100
+            leftMotor = forward#(255 * forward)/100
+            rightMotor = turn#(255 * turn)/100
           elif (forward > threshold and turn < -threshold):
             #go slight left
             direction = 1
-            rightMotor = (255 * forward)/100
-            leftMotor = (255 * (turn * -1))/100
+            rightMotor = forward#(255 * forward)/100
+            leftMotor = (turn * -1)#(255 * (turn * -1))/100
           elif (turn < -threshold and forward < threshold and forward > -threshold):
             #go slight left
             direction = 0
             leftMotor = 0
-            rightMotor = (255 * (turn * -1))/100 
+            rightMotor = (turn * -1)#(255 * (turn * -1))/100 
           elif (turn > threshold and forward < threshold and forward > -threshold):
             #go slight left
             direction = 0     
             rightMotor = 0
-            leftMotor = (255 * turn)/100 
+            leftMotor = turn#(255 * turn)/100 
           else:
             direction = 0
             leftMotor = 0
             rightMotor = 0
 
           print leftMotor, " <<>> ", rightMotor, " <<>> ", direction
+
+          if (direction == 0):
+            drive("f", str(leftMotor), str(rightMotor))
+          elif (direction == 1):
+            drive("b", str(leftMotor), str(rightMotor))
 
 except KeyboardInterrupt:
     # Turn off the motors
