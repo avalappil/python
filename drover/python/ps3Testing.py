ab9db6653b3dda52d44fd7594f8d5f82028e73c6
Ajith Valappil <ajith.vv@gmail.com>
2015-02-15 12:16:21 -0600

    Sun 12:16

---
+++ drover/python/ps3Testing.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import pygame
+import time
+import serial
+
+# initialize Serial driver
+serialC = serial.Serial('/dev/ttyAMA0', 9600)
+
+# Initialise the pygame library
+pygame.init()
+
+# Connect to the first JoyStick
+j = pygame.joystick.Joystick(0)
+j.init()
+
+threshold = 0.30
+
+print 'Initialized Joystick : %s' % j.get_name()
+
+
+typeForArduino = ""
+dataForArduino = ""
+
+typeForServo = ""
+dataForServo = ""
+
+# Configure the motors to match the current settings.
+def drive(typ, data):
+  print data
+  serialC.write(typ)
+  serialC.write(data)
+  serialC.write("#")
+
+# Try and run the main code, and in case of failure we can stop the motors
+try:
+    # This is the main loop
+    while True:
+      time.sleep(1)
+      events = pygame.event.get()
+      for event in events:
+        UpdateMotors = 1
+        forward = 0
+        turn = 0
+        UpdateServo = 0
+        up = 0
+        rotate = 0
+        # Check if one of the joysticks has moved
+        if event.type == pygame.JOYAXISMOTION:
+          forward = j.get_axis(3)
+          turn = j.get_axis(2)
+          forward = float("{0:.2f}".format(forward))
+          turn = float("{0:.2f}".format(turn))
+          print forward, " <<>> ", turn
+          ##### right joy stick - robot control
+          #if event.axis == 2:
+          #  turn = float("{0:.2f}".format(event.value))
+          #  UpdateMotors = 1
+          #elif event.axis == 3:
+          #  forward = float("{0:.2f}".format(event.value))
+          #  UpdateMotors = 1
+
+          # Check if we need to update what the motors are doing
+          if UpdateMotors:
+            if (forward > threshold):
+              print "reverse: "
+              print forward
+              drive("f",str(forward))
+            elif (forward < -threshold):
+              print "forward: "
+              print  forward
+              drive("f",str(forward))
+            elif (turn > threshold):
+              print "right: "
+              print turn
+              drive("t",str(turn))
+            elif (turn < -threshold):
+              print "left: "
+              print turn
+              drive("t",str(turn))
+
+except KeyboardInterrupt:
+    # Turn off the motors
+    j.quit()#!/usr/bin/env python
